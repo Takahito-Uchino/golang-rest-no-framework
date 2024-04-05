92fc3244f98c367f907a5d8b73f7de72f24b7670
Taka <takahito.uchino.1218@gmail.com>
2024-04-05 19:00:19 +0900

    データベース設定

---
+++ postgres/init/init.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS todos;
+
+CREATE TABLE todos (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(40) NOT NULL,
+    content TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
+);
+
+CREATE OR REPLACE FUNCTION update_modified_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = now();
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_modified_column
+BEFORE UPDATE ON todos
+FOR EACH ROW
+EXECUTE FUNCTION update_modified_column();
+
+INSERT INTO todos (title, content) VALUES('todo1', 'aaa');
